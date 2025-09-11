0898c3fc97751ef8f4161d58bbf0addbed995080
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-11 18:28:55 +0300

    fix: fixed login page

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,28 +2,28 @@ import ForestIcon from '@mui/icons-material/Forest';
 
 export default function login() {
   return (
-    <div className="flex justify-center items-center w-full h-screen bg-gray-300">
-        <div className='flex top-0 fixed'>
-            <ForestIcon className='m-4' />
-            <h1 className='py-4'>ASJA University</h1>
+    <div className="flex justify-center items-center w-full h-screen">
+        <div className='flex m-2 rounded-full bg-white top-3 left-10 fixed'>
+            <ForestIcon className='my-2 ml-4 mr-2' />
+            <h1 className='py-2 pr-4'>ASJA University</h1>
         </div>
       <div className="flex w-1/2 h-screen flex-col justify-center items-center">
         <div className="p-10 bg-white">
             <h1 className="text-4xl p-5 font-bold text-teal-800">Espace etudiant</h1>
-            <p className="pl-5 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            <p className="pl-5 pb-5 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>                      
+        </div>
       </div>
       <div className="flex w-1/2 h-screen justify-center items-center">
         <div className="flex-col w-100 justify-center items-center bg-white p-10">
-            <h1 className="flex mb-5 text-2xl font-bold text-teal-800">Connexion</h1>
+            <h1 className="flex justify-center mb-5 text-2xl font-bold text-teal-800">Connexion</h1>
             <label >Email</label>
-            <input className="flex w-full mb-5 border-teal-800 border-2 p-2" type="email" />
+            <input className="flex w-full mb-5 border-teal-800 border-2 rounded-md p-2" type="email" />
             <label >Mot de passe</label>
-            <input className="flex w-full mb-5 border-teal-800 border-2 p-2" type="password" />
-            <div className="flex justify-center hover:bg-teal-600 hover:scale-101 duration-300 cursor-poi bg-teal-800 text-white p-3">
-                <button>se connecter</button>
-            </div>
+            <input className="flex w-full mb-5 border-teal-800 border-2 rounded-md p-2" type="password" />
+            <button className="flex w-full justify-center hover:bg-teal-600 shadow-lg shadow-gray-400 hover:scale-101 duration-300 rounded-full cursor-pointer bg-teal-800 text-white p-3">
+                se connecter
+            </button>
             <div className="flex justify-center">
                 <hr className="flex  w-1/2 border-teal-800 m-5" />
             </div>
