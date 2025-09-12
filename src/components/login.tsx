1b6e9a3306a921e09e9d6620120866b684c17eb3
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-12 10:25:38 +0300

    fix: remove outline focus

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,8 +2,8 @@ import ForestIcon from '@mui/icons-material/Forest';
 
 export default function login() {
   return (
-    <div className="flex justify-center items-center w-full h-screen">
-        <div className='flex m-2 rounded-full bg-white top-3 left-10 fixed'>
+    <div className="flex justify-center items-center w-full h-screen bg-white">
+        <div className='flex m-2 rounded-full top-3 left-10 fixed'>
             <ForestIcon className='my-2 ml-4 mr-2' />
             <h1 className='py-2 pr-4'>ASJA University</h1>
         </div>
@@ -15,13 +15,13 @@ export default function login() {
         </div>
       </div>
       <div className="flex w-1/2 h-screen justify-center items-center">
-        <div className="flex-col w-100 justify-center items-center bg-white p-10">
+        <div className="flex-col w-100 justify-center items-center p-10">
             <h1 className="flex justify-center mb-5 text-2xl font-bold text-teal-800">Connexion</h1>
             <label >Email</label>
-            <input className="flex w-full mb-5 border-teal-800 border-2 rounded-md p-2" type="email" />
+            <input className="flex w-full mb-5 border-gray-300 border-2 focus:ring-0 focus:outline-none rounded-md p-2 " type="email" />
             <label >Mot de passe</label>
-            <input className="flex w-full mb-5 border-teal-800 border-2 rounded-md p-2" type="password" />
-            <button className="flex w-full justify-center hover:bg-teal-600 shadow-lg shadow-gray-400 hover:scale-101 duration-300 rounded-full cursor-pointer bg-teal-800 text-white p-3">
+            <input className="flex w-full mb-5 border-gray-300 border-2 focus:ring-0 focus:outline-none rounded-md p-2" type="password" />
+            <button className="flex w-full justify-center focus:bg-teal-600 hover:bg-teal-600 shadow-lg shadow-gray-400 hover:scale-101 duration-300 rounded-full cursor-pointer bg-teal-800 text-white p-3">
                 se connecter
             </button>
             <div className="flex justify-center">
