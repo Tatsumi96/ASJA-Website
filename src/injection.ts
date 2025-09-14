826790334fb967cc488a1d7766bef9b314bb6dc1
Dera2S <dera.ah.14@gmail.com>
2025-09-14 10:47:18 +0300

    feat : injection of auth features

--- src/injection.ts
+++ src/injection.ts
@@ -1,44 +1,15 @@
-import { ProductMemoryStorage } from "./application/database/ProductMemorySource";
-import { UserMemoryStorage } from "./application/database/UserMemoryStorage";
-import { OrderRepositoryImpl } from "./application/repository/OrderRepositoryImpl";
-import { ProductRepositoryImp } from "./application/repository/ProductRepository";
-import { UserRepositoryImp } from "./application/repository/UserRepository";
-import { AddProductToTheOrder } from "./application/use-cases/Order/addProductOrder";
-import { CreateAnOrder } from "./application/use-cases/Order/createAnOrder";
-import { RemoveProductOrder } from "./application/use-cases/Order/removeProduct";
-import { FilterAndSortProductsUseCase } from "./application/use-cases/Product/FilterAndSortProducts";
-import { GetAllProduct } from "./application/use-cases/Product/GetAllProduct";
-import { GetOneProduct } from "./application/use-cases/Product/GetOneProduct";
-import { InsertManyProduct } from "./application/use-cases/Product/InsertManyProduct";
-import { InsertOneProduct } from "./application/use-cases/Product/insertOneProduct";
-import { FindUserUseCase } from "./application/use-cases/User/findUser";
+import axios from "axios";
+import { AuthServiceImpl } from "./features/auth/auth_service";
+import { AuthRepositoryImpl } from "./features/auth/auth_repo_impl";
+import { LogUseCase } from "./features/auth/usecases/logUseCase";
 
-// Database
-const productMemoryStorage = new ProductMemoryStorage();
-const userMemoryStorage = new UserMemoryStorage();
+const api = axios.create({
+  timeout: 5000,
+  withCredentials: true,
+});
 
-// Repositories
-const productRepository = new ProductRepositoryImp(productMemoryStorage);
-const orderRepository = new OrderRepositoryImpl(productMemoryStorage);
-const userRepository = new UserRepositoryImp(userMemoryStorage);
+const authService = new AuthServiceImpl(api);
 
-// Product Use Cases
-export const getAllProductUseCase = new GetAllProduct(productRepository);
-export const getOneProductUseCase = new GetOneProduct(productRepository);
-export const insertManyProductUseCase = new InsertManyProduct(
-  productRepository
-);
-export const insertOneProductUseCase = new InsertOneProduct(productRepository);
+const authRepository = new AuthRepositoryImpl(authService);
 
-// Order Use Cases
-export const addProductToTheOrderUseCase = new AddProductToTheOrder(
-  orderRepository
-);
-export const createAnOrderUseCase = new CreateAnOrder(orderRepository);
-export const removeProductOrderUseCase = new RemoveProductOrder(
-  orderRepository
-);
-
-export const findUserUseCase = new FindUserUseCase(userRepository);
-
-export const filterAndSortProduct = new FilterAndSortProductsUseCase();
+export const logUseCase = new LogUseCase(authRepository);
