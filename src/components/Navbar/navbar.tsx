4e82c070f8c70a79251940b79368c5f5831af075
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-15 18:33:40 +0300

    update navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,17 +3,11 @@ import ForestIcon from '@mui/icons-material/Forest';
 export default function Navbar() {
     
   return (
-<<<<<<< HEAD:src/components/Navbar/navbar.tsx
-    <div className="flex top-0 fixed w-full shadow-md bg-teal-800 text-white z-50">
-        <ForestIcon className='m-4' />
-        <a className='py-4'>ASJA University</a>
-=======
     <div className="flex top-0 fixed w-full shadow-md bg-white text-teal-800 z-50">
         <div className='flex rounded-full ml-10 cursor-pointer' onClick={() => window.location.href = '/'}>
           <ForestIcon className='my-4 ml-4 mr-2' />
           <h1 className='py-4 text-black'>ASJA University</h1>
         </div>
->>>>>>> 95ef091 (fix : update design):src/components/header.tsx
         <div className='flex fixed justify-center items-center right-10 '>
             <button className='px-5 py-4 cursor-pointer font-bold hover:bg-gray-300 '>À propos</button>
             <button className='px-5 py-4 cursor-pointer font-bold hover:bg-gray-300'>Admission</button>
