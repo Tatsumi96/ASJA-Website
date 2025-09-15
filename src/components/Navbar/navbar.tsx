04d7bc6aafbc049fdf239938cd794164195b363d
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-15 22:36:06 +0300

    add/fix front

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,19 +1,19 @@
-import ForestIcon from '@mui/icons-material/Forest';
+import Logo from '../../assets/Logo/asja-logo.jpg'
 
 export default function Navbar() {
     
   return (
-    <div className="flex top-0 fixed w-full shadow-md bg-white text-teal-800 z-50">
-        <div className='flex rounded-full ml-10 cursor-pointer' onClick={() => window.location.href = '/'}>
-          <ForestIcon className='my-4 ml-4 mr-2' />
-          <h1 className='py-4 text-black'>ASJA University</h1>
-        </div>
+    <div className="flex top-0 fixed w-full shadow-md bg-white text-gray-800 z-50">
+        <a className='flex ml-10 cursor-pointer' onClick={() => window.location.href = '/'}>
+          <img className='m-2 w-14 h-14'  src={Logo}/>
+          <h1 className='py-5 mt-1 text-gray-900 font-bold'>ASJA University</h1>
+        </a>
         <div className='flex fixed justify-center items-center right-10 '>
-            <button className='px-5 py-4 cursor-pointer font-bold hover:bg-gray-300 '>À propos</button>
-            <button className='px-5 py-4 cursor-pointer font-bold hover:bg-gray-300'>Admission</button>
-            <button className='px-5 py-4 cursor-pointer font-bold hover:bg-gray-300'>Filiaire</button>
-            <button className='px-5 py-4 cursor-pointer font-bold hover:bg-gray-300'>Contact</button>
-            <a onClick={() => window.location.href = '/Login'}><div className='px-5 py-2 my-2 ml-3 text-white font-bold bg-teal-800 rounded-full cursor-pointer hover:bg-gray-300 hover:scale-110 duration-300'>
+            <button className='px-5 py-6 cursor-pointer font-bold hover:bg-gray-300 '>À propos</button>
+            <button className='px-5 py-6 cursor-pointer font-bold hover:bg-gray-300'>Admission</button>
+            <button className='px-5 py-6 cursor-pointer font-bold hover:bg-gray-300'>Filiaire</button>
+            <button className='px-5 py-6 cursor-pointer font-bold hover:bg-gray-300'>Contact</button>
+            <a onClick={() => window.location.href = '/Login'}><div className='px-6 py-3 mt-1 ml-3 text-yellow-500 font-bold bg-gray-800 rounded-full cursor-pointer hover:bg-gray-600 hover:scale-105 duration-300 shadow-md shadow-gray-600'>
                 Postuler
             </div></a>
         </div>
--- src/page/login.tsx
+++ src/page/login.tsx
@@ -1,36 +1,36 @@
-import ForestIcon from '@mui/icons-material/Forest';
+import Logo from '../assets/Logo/asja-logo.jpg'
 
 export default function login() {
   return (
     <div className="flex justify-center items-center w-full h-screen bg-white">
         <a onClick={() => window.location.href = '/'}>
           <div className='flex m-2 rounded-full top-3 left-10 fixed'>
-            <ForestIcon className='my-2 ml-4 mr-2' />
-            <h1 className='py-2 pr-4'>ASJA University</h1>
+            <img className='w-13 h-13 ' src={Logo} />
+            <h1 className='py-3 pr-4 ml-3 font-bold text-gray-800'>ASJA University</h1>
           </div>
         </a>
       <div className="flex w-1/2 h-screen flex-col justify-center items-center">
         <div className="p-10 bg-white">
-            <h1 className="text-4xl p-5 font-bold text-teal-800">Espace etudian</h1>
+            <h1 className="text-4xl p-5 font-bold text-gray-800">Espace etudiant</h1>
             <p className="pl-5 pb-5 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </div>
       <div className="flex w-1/2 h-screen justify-center items-center">
         <div className="flex-col w-100 justify-center items-center p-10">
-            <h1 className="flex justify-center mb-5 text-2xl font-bold text-teal-800">Connexion</h1>
+            <h1 className="flex justify-center mb-5 text-2xl font-bold text-gray-800">Connexion</h1>
             <label >Email</label>
-            <input className="flex w-full mb-5 border-gray-300 border-2 focus:ring-0 focus:outline-none rounded-md p-2 " type="email" />
+            <input className="flex w-full mb-5 border-gray-300 border-2 focus:ring-0 focus:outline-none rounded-md p-2.5 " type="email" />
             <label >Mot de passe</label>
-            <input className="flex w-full mb-5 border-gray-300 border-2 focus:ring-0 focus:outline-none rounded-md p-2" type="password" />
-            <button className="flex w-full justify-center focus:bg-teal-600 hover:bg-teal-600 shadow-lg shadow-gray-400 hover:scale-101 duration-300 rounded-full cursor-pointer bg-teal-800 text-white p-3">
+            <input className="flex w-full mb-5 border-gray-300 border-2 focus:ring-0 focus:outline-none rounded-md p-2.5" type="password" />
+            <button className="flex w-full justify-center focus:bg-gray-600 hover:bg-gray-600 shadow-lg shadow-gray-400 hover:scale-101 duration-300 rounded-full cursor-pointer bg-gray-900 text-yellow-500 p-3.5">
                 se connecter
             </button>
             <div className="flex justify-center">
                 <hr className="flex  w-1/2 border-teal-800 m-5" />
             </div>
             <div className="flex justify-center text-sm"><span className="">Pas encore Etudiant ?</span></div>
-            <a className="flex justify-center cursor-pointer mt-2  text-teal-800 hover:underline text-md ">
+            <a className="flex justify-center cursor-pointer mt-2  text-yellow-800 hover:underline text-md ">
                 S'inscrire
             </a>
         </div>
