803858f2790b2db3141d9e1d5caaff36a3df25aa
Dera2S <dera.ah.14@gmail.com>
2025-09-19 23:50:54 +0300

    feat : add student space component

---
+++ src/page/student_space/index.tsx
@@ -0,0 +1,18 @@
+import { NotificationWithTimer } from "@/components/ui/notification";
+import { StudentPortalProvider } from "./bloc/useStudentPortalProvider";
+import { FileAndNewsSection } from "./components/files_news_section";
+import { NavBar } from "./components/nav_bar";
+import { StatisSection } from "./components/statis_section";
+
+export const StudentSpacePage = () => {
+  return (
+    <StudentPortalProvider>
+      <div className=" bg-zinc-100 flex flex-col">
+        <NavBar />
+        <StatisSection />
+        <FileAndNewsSection />
+      </div>
+      <NotificationWithTimer />
+    </StudentPortalProvider>
+  );
+};
