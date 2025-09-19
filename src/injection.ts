54ae42340c1d82431e445b07b15fd811545c3075
Dera2S <dera.ah.14@gmail.com>
2025-09-19 23:54:00 +0300

    feat : add doc features

--- src/injection.ts
+++ src/injection.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { AuthServiceImpl } from "./features/auth/auth_service";
 import { AuthRepositoryImpl } from "./features/auth/auth_repo_impl";
 import { LogUseCase } from "./features/auth/usecases/logUseCase";
+import { DocServiceImpl } from "./features/doc/doc_service";
+import { DocRepositoryImpl } from "./features/doc/doc.repositoryImpl";
 
 const api = axios.create({
   timeout: 5000,
@@ -10,6 +12,10 @@ const api = axios.create({
 
 const authService = new AuthServiceImpl(api);
 
+const docService = new DocServiceImpl(api);
+
 const authRepository = new AuthRepositoryImpl(authService);
+
+export const docRepo = new DocRepositoryImpl(docService);
 
 export const logUseCase = new LogUseCase(authRepository);
