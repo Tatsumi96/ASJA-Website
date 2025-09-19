22f2bef4abe4001862c954b4dacd8e1076ce5ef8
Dera2S <dera.ah.14@gmail.com>
2025-09-20 00:01:32 +0300

    fix : separate route and rename app name

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,7 @@
-import { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
-import { LandingPage } from "./page/landing";
-import { LogInSection } from "./page/login";
+import { PageRoute } from "./routes";
 
-interface LogPayload {
-  message: string;
-  timestamp: string;
-}
-const App: React.FC = () => {
-  useEffect(() => {
-    const sendLog = async () => {
-      try {
-        const payload = {
-          identifier: "0388257986",
-          password: "1234",
-        };
-
-        const response = await fetch("http://localhost:5000/auth/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-        });
-
-        if (!response.ok) {
-          throw new Error("Erreur réseau");
-        }
-
-        const data: unknown = await response.json();
-        console.log("Log envoyé au serveur:", data);
-      } catch (error) {
-        console.error("Erreur lors de l'envoi du log:", error);
-      }
-    };
-
-    sendLog();
-  }, []);
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/Login" element={<LogInSection />} />
-        </Routes>
-      </Router>
-    </>
-  );
+const AsjaWebApp: React.FC = () => {
+  return <PageRoute />;
 };
-export default App;
-
-// test
+export default AsjaWebApp;
