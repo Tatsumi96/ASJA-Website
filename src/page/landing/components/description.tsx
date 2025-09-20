b890397aa2617185d7bef7202185c9a8d2252dbf
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-20 12:42:20 +0300

    fix : syncronised main & Front

--- src/page/landing/components/description.tsx
+++ src/page/landing/components/description.tsx
@@ -1,57 +1,27 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import backgroundImage from "@/assets/Lieu_espace/asja_couloir.jpg";
-
 export const Description = () => {
   return (
-    <section className="flex h-screen w-full flex-row z-0">
+    <section className="flex h-screen w-screen flex-row z-0">
       <div className="flex flex-col justify-center p-10 w-1/2 bg-white">
-        <h1 className="text-6xl text-green-700 mb-10">
-          Athénée Saint Joseph Antsirabe
-        </h1>
-        <p className="text-2xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <button className="bg-green-800 hover:bg-green-900 cursor-pointer hover:scale-105 duration-300 w-1/4 py-4 rounded-full mt-10">
-          <a href="/about" className="text-white font-bold text-lg">
-            En savoir plus →
-          </a>
-=======
-import Image from "../../../assets/Lieu_espace/vue_aerienne_asja.jpg"
-=======
-import Image from "../../../assets/Lieu_espace/asja_couloir.jpg"
->>>>>>> 08df7cf (modif: background image)
-
-export const Description = () => {
-  return (
-    <section className="flex h-screen w-screen flex-row z-0 bg-white">
-      <div className="flex flex-col justify-center px-20 p-10 w-1/2">
         <h1 className="text-6xl text-green-700 mb-10" >Athénée Saint Joseph Antsirabe</h1>
-        <p className="text-2xl">Qui propose une formation d'excellence aux étudiants désireux de devenir 
-        les managers et décideurs de demain.</p>
+        <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <button 
-          className="flex items-center justify-center w-max bg-green-800 hover:bg-green-900 cursor-pointer hover:scale-105 duration-300 py-4 rounded-full mt-10">
+          className="bg-green-800 hover:bg-green-900 cursor-pointer hover:scale-105 duration-300 w-1/4 py-4 rounded-full mt-10">
           <a 
             href="/about" 
-            className="text-white font-bold text-lg mx-5"
+            className="text-white font-bold text-lg"
             >
               En savoir plus →
             </a>
->>>>>>> 20bcafa (modif: used chart graphics)
         </button>
       </div>
       <div className="flex flex-col justify-center items-center pl-10 w-1/2 bg-gray-200 rounded-l-full">
-        <div></div>
-<<<<<<< HEAD
         <img
           className="h-full w-max rounded-l-full "
           src={backgroundImage}
           alt=""
         />
-=======
-        <img className="h-full w-max rounded-l-full " src={Image} alt="" />
->>>>>>> 20bcafa (modif: used chart graphics)
       </div>
     </section>
   );
--- src/page/landing/components/navbar.tsx
+++ src/page/landing/components/navbar.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from 'react-router-dom'
 import Logo from "@/assets/Logo/asja-logo.png";
-import { NavLink } from "react-router-dom";
 
 
 export const Navbar = () => {
@@ -16,60 +15,6 @@ export const Navbar = () => {
         </h1>
       </a>
       <div className="flex fixed justify-center items-center right-10 ">
-<<<<<<< HEAD
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "text-green-700 bg-green-50 px-4 py-6 rounded font-medium"
-              : "text-gray-800 hover:text-stone-500 px-4 py-6"
-          }
-        >
-          Accueil
-        </NavLink>
-
-        <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            isActive
-              ? "text-green-700 bg-green-50 px-4 py-2 rounded font-medium"
-              : "text-gray-800 hover:text-stone-500 px-4 py-2 rounded"
-          }
-        >
-          A propos
-        </NavLink>
-
-        <NavLink
-          to="/programmes"
-          className={({ isActive }) =>
-            isActive
-              ? "text-green-700 bg-green-50 px-4 py-2 rounded font-medium"
-              : "text-gray-800 hover:text-stone-500 px-4 py-2 rounded"
-          }
-        >
-          Programmes
-        </NavLink>
-
-        <NavLink
-          to="/contact"
-          className={({ isActive }) =>
-            isActive
-              ? "text-green-700 bg-green-50 px-4 py-2 rounded font-medium"
-              : "text-gray-800 hover:text-stone-500 px-4 py-2 rounded"
-          }
-        >
-          Contact
-        </NavLink>
-
-        <a onClick={() => (window.location.href = "/Login")}>
-          <div
-            className="px-6 py-4 my-1 ml-3 text-white font-bold bg-green-700
-                           rounded-full cursor-pointer hover:bg-green-800 
-                           hover:scale-105 duration-300"
-          >
-            Postuler
-          </div>
-=======
       <NavLink 
             to="/"
             className={({ isActive }) => 
@@ -123,7 +68,6 @@ export const Navbar = () => {
                 Postuler
             </div></a>
         <a onClick={() => (window.location.href = "/Login")}>
->>>>>>> 20bcafa (modif: used chart graphics)
         </a>
       </div>
     </div>
