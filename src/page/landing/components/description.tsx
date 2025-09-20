6cd9b391dfbfeba5f2ce48a5ddc074280f8a8dfb
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-20 10:12:21 +0300

    modif: used chart graphics

--- src/page/landing/components/description.tsx
+++ src/page/landing/components/description.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import backgroundImage from "@/assets/Lieu_espace/asja_couloir.jpg";
 
 export const Description = () => {
@@ -15,15 +16,38 @@ export const Description = () => {
           <a href="/about" className="text-white font-bold text-lg">
             En savoir plus →
           </a>
+=======
+import Image from "../../../assets/Lieu_espace/vue_aerienne_asja.jpg"
+
+export const Description = () => {
+  return (
+    <section className="flex h-screen w-screen flex-row z-0">
+      <div className="flex flex-col justify-center p-10 w-1/2 bg-white">
+        <h1 className="text-6xl text-green-700 mb-10" >Athénée Saint Joseph Antsirabe</h1>
+        <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button 
+          className="bg-green-800 hover:bg-green-900 cursor-pointer hover:scale-105 duration-300 w-1/4 py-4 rounded-full mt-10">
+          <a 
+            href="/about" 
+            className="text-white font-bold text-lg"
+            >
+              En savoir plus →
+            </a>
+>>>>>>> 20bcafa (modif: used chart graphics)
         </button>
       </div>
       <div className="flex flex-col justify-center items-center pl-10 w-1/2 bg-gray-200 rounded-l-full">
         <div></div>
+<<<<<<< HEAD
         <img
           className="h-full w-max rounded-l-full "
           src={backgroundImage}
           alt=""
         />
+=======
+        <img className="h-full w-max rounded-l-full " src={Image} alt="" />
+>>>>>>> 20bcafa (modif: used chart graphics)
       </div>
     </section>
   );
