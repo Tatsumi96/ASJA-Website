41b87e8d348204933b8503c8a7e26345fe4b5344
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-20 12:45:19 +0300

    modif : h1 in description.tsx

--- src/page/landing/components/description.tsx
+++ src/page/landing/components/description.tsx
@@ -4,8 +4,8 @@ export const Description = () => {
     <section className="flex h-screen w-screen flex-row z-0">
       <div className="flex flex-col justify-center p-10 w-1/2 bg-white">
         <h1 className="text-6xl text-green-700 mb-10" >Athénée Saint Joseph Antsirabe</h1>
-        <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p className="text-2xl">Qui propose une formation d'excellence aux étudiants désireux de devenir 
+        les managers et décideurs de demain.</p>
         <button 
           className="bg-green-800 hover:bg-green-900 cursor-pointer hover:scale-105 duration-300 w-1/4 py-4 rounded-full mt-10">
           <a 
