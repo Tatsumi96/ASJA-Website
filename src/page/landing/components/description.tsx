a8b21200a8bd6ae1e91f3a48c20bd2951df8ef4b
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-20 11:04:53 +0300

    modif: modified section

--- src/page/landing/components/description.tsx
+++ src/page/landing/components/description.tsx
@@ -25,16 +25,16 @@ import Image from "../../../assets/Lieu_espace/asja_couloir.jpg"
 
 export const Description = () => {
   return (
-    <section className="flex h-screen w-screen flex-row z-0">
-      <div className="flex flex-col justify-center p-10 w-1/2 bg-white">
+    <section className="flex h-screen w-screen flex-row z-0 bg-white">
+      <div className="flex flex-col justify-center px-20 p-10 w-1/2">
         <h1 className="text-6xl text-green-700 mb-10" >Athénée Saint Joseph Antsirabe</h1>
-        <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p className="text-2xl">Qui propose une formation d'excellence aux étudiants désireux de devenir 
+        les managers et décideurs de demain.</p>
         <button 
-          className="bg-green-800 hover:bg-green-900 cursor-pointer hover:scale-105 duration-300 w-1/4 py-4 rounded-full mt-10">
+          className="flex items-center justify-center w-max bg-green-800 hover:bg-green-900 cursor-pointer hover:scale-105 duration-300 py-4 rounded-full mt-10">
           <a 
             href="/about" 
-            className="text-white font-bold text-lg"
+            className="text-white font-bold text-lg mx-5"
             >
               En savoir plus →
             </a>
