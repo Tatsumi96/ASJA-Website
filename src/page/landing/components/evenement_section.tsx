a6749c2f7a06ced5937bea6c7032a5502ade7b68
Dera2S <dera.ah.14@gmail.com>
2025-09-22 11:53:03 +0300

    fix : make evenement text green and bold

--- src/page/landing/components/evenement_section.tsx
+++ src/page/landing/components/evenement_section.tsx
@@ -1,6 +1,6 @@
 export default function evenement_section() {
   return (
-    <div className="h-screen w-full flex justify-center items-center">
+    <div className="h-screen w-full flex justify-center items-center text-green-700 text-5xl font-bold">
       <h1>evenement_section</h1>
     </div>
   )
