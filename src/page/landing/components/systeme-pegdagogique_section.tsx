df2161ebb89b8c0e766c4ff05bd73f183e89092b
MandaHarou <andrainiony0109@gmail.com>
2025-09-25 16:14:19 +0300

    feature: add section padagogique systeme, its just disposition

---
+++ src/page/landing/components/systeme-pegdagogique_section.tsx
@@ -0,0 +1,51 @@
+export const SystemePedagogiqueSection = () => {
+  return (
+    <div className="w-full bg-white dark:bg-zinc-800 dark:text-white p-10">
+      <h1 className="text-4xl font-bold text-center mb-8">
+        Systeme Pédagogique
+      </h1>
+
+   
+      <div className="flex flex-col md:flex-row gap-15 mb-6">
+    
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold mb-4">Titre 1</h2>
+          <p className="text-base leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Voluptatibus qui 
+            esse repellat maxime quasi placeat dolore.
+          </p>
+        </div>
+
+      
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold mb-4">Titre 2</h2>
+          <p className="text-base leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ea facilis 
+            reprehenderit magni voluptas officiis deserunt.
+          </p>
+        </div>
+      </div>
+
+   
+      <div className="flex flex-col md:flex-row gap-15">
+    
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold mb-4">Titre 3</h2>
+          <p className="text-base leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Magnam fugiat dolores minima.
+          </p>
+        </div>
+
+      
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold mb-4">Titre 4</h2>
+          <p className="text-base leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Consequatur, nulla.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
