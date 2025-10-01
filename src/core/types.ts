8126dac6e632db21d2ca11995b842b7b9387bcbe
Dera2S <dera.ah.14@gmail.com>
2025-10-01 15:18:35 +0300

    feat : adding type

---
+++ src/core/types.ts
@@ -0,0 +1,37 @@
+export type Level = "L1" | "L2" | "L3" | "M1" | "M2";
+
+export type Role = "User" | "Admin";
+
+export type Mention =
+  | "INFORMATIQUE"
+  | "DROIT"
+  | "AGRONOMIE"
+  | "ECONOMIE"
+  | "LANGUE ET CULTURE"
+  | "SCIENCE DE LA TERRE";
+
+export type Branche =
+  | "GL"
+  | "GI"
+  | "TCO"
+  | "AFFAIRES"
+  | "PROCESSUEL"
+  | "HYDROGEOLOGIE"
+  | "GEOLOGIE MINIERE"
+  | "ECONOMIE ET DEVELOPPEMENT"
+  | "GESTION ET COMMERCE INTERNATIONAL"
+  | "AGROALIMENTAIRE"
+  | "PRODUCTION VEGETALE"
+  | "PRODUCTION ANIMAL"
+  | "COMMUN";
+
+export const mentions: Record<string, string[]> = {
+  INFORMATIQUE: ["GL", "TCO", "GI"],
+  DROIT: ["PROCESSUEL", "AFFAIRES"],
+  ECONOMIE: ["GESTION ET COMMERCE INTERNATIONAL", "ECONOMIE ET DEVELOPPEMENT"],
+  AGRNONOMIE: ["AGROALIMENTAIRE", "PRODUCTION VEGETALE", "PRODUCTION ANIMAL"],
+  SCIENCE_DE_LA_TERRE: ["HYDROGEOLOGIE", "GEOLOGIE MINIERE"],
+  LANGUE_ET_CULTURE: [],
+};
+
+export const classes = ["L1", "L2", "L3", "M1", "M2"];
