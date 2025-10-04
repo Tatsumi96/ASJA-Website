de59b13aa4516276d4cf8a15a43b35f4629e2b3a
Dera2S <dera.ah.14@gmail.com>
2025-10-04 10:32:38 +0300

    fix : remove registration

--- src/features/user/user.repository.ts
+++ src/features/user/user.repository.ts
@@ -1,8 +1,6 @@
 import type { Result } from "@/core/result";
 import type { UserDto } from "./user.dto";
-import type { UserEntity } from "./user.entity";
 
 export abstract class UserRepository {
   abstract getData(): Promise<Result<UserDto>>;
-  abstract register(user: UserEntity): Promise<Result<void>>;
 }
