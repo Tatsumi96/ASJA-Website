2c4c6c5b8bdd08015ca34c110edd8f757b0ffc16
Dera2S <dera.ah.14@gmail.com>
2025-10-04 15:00:08 +0300

    feat : add bar chart

--- src/page/admin/components/MentionListCard.tsx
+++ src/page/admin/components/MentionListCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { MdBook } from "react-icons/md";
+import { BarChartGraph } from "./BarChart";
 
 const MentionCart = ({
   className,
@@ -11,12 +11,12 @@ const MentionCart = ({
   return (
     <div className="w-full flex p-5 justify-around">
       <Card
-        className={` bg-gradient-to-r ${className} text-white w-full text-2xl cursor-pointer font-semibold`}
+        className={` ${className} text-black  bg-white dark:bg-transparent  dark:text-white  w-full text-2xl cursor-pointer font-semibold`}
       >
         <CardContent>
-          <div>
-            <MdBook className=" text-5xl" /> {mention}
-          </div>
+          <p className=" text-3xl">{mention}</p>
+          <p className=" text-lg py-7">3500 etudiants</p>
+          <BarChartGraph/>
         </CardContent>
       </Card>
     </div>
@@ -26,26 +26,20 @@ const MentionCart = ({
 export const MentionCardList = () => {
   return (
     <div className="flex">
-      <MentionCart mention="Droit" className="from-red-700 to-red-600" />
+      <MentionCart mention="Droit" className="border-red-600 border-4" />
       <MentionCart
         mention="Informatique"
-        className="from-violet-800 to-violet-700"
+        className="border-violet-600 border-4"
       />
-      <MentionCart
-        mention="Economie"
-        className="from-yellow-500 to-yellow-400"
-      />
-      <MentionCart
-        mention="Agronomie"
-        className="from-green-500 to-green-600"
-      />
+      <MentionCart mention="Economie" className="border-yellow-600 border-4" />
+      <MentionCart mention="Agronomie" className="border-green-600 border-4" />
       <MentionCart
         mention="Langue Etrangere Applique"
-        className="from-blue-500 to-blue-600"
+        className="border-blue-600 border-4"
       />
       <MentionCart
         mention="Science de la Terre"
-        className="from-gray-500 to-gray-600"
+        className="border-gray-600 border-4"
       />
     </div>
   );
--- src/page/admin/index.tsx
+++ src/page/admin/index.tsx
@@ -6,7 +6,7 @@ import { NavBar } from "./components/nav_bar";
 export const AdminDashboardPage = () => {
   return (
     <AdminDashBoardProvider>
-      <div className="flex flex-col w-full gap-3 p-4 pt-15 bg-zinc-100  dark:bg-zinc-900 transition-all h-screen duration-500 ">
+      <div className="flex flex-col w-full gap-3 p-4 pt-15 bg-zinc-100  dark:bg-zinc-900 transition-all h-full duration-500 ">
         <NavBar />
         <MentionCardList />
         <AddUserAndDocument />
