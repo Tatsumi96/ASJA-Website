d8feff80aa6810923ce8623cfa006e11825c1351
Dera2S <dera.ah.14@gmail.com>
2025-10-05 09:52:30 +0300

    feat : handle data from server and a fake data

--- src/page/admin/components/BarChart.tsx
+++ src/page/admin/components/BarChart.tsx
@@ -6,26 +6,70 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import type { MentionDto } from "@/features/mention/mention.dto";
+import type { Mention } from "@/core/types";
 
-const chartData = [
-  { Niveau: "L1", Etudiant: 186 },
-  { Niveau: "L2", Etudiant: 305 },
-  { Niveau: "L3", Etudiant: 237 },
-  { Niveau: "M1", Etudiant: 73 },
-  { Niveau: "M2", Etudiant: 209 },
-];
+export const BarChartGraph = ({
+  mention,
+  color,
+  item,
+}: {
+  mention: Mention;
+  color: string;
+  item: MentionDto;
+}) => {
+  const chartData = [
+    {
+      Niveau: "L1",
+      Etudiant:
+        item?.[mention.replace(/ /g, "_") as keyof MentionDto]?.data[0]
+          ?.studentNumber ?? 0,
+    },
+    {
+      Niveau: "L2",
+      Etudiant:
+        item?.[mention.replace(/ /g, "_") as keyof MentionDto]?.data[1]
+          ?.studentNumber ?? 0,
+    },
+    {
+      Niveau: "L3",
+      Etudiant:
+        item?.[mention.replace(/ /g, "_") as keyof MentionDto]?.data[2]
+          ?.studentNumber ?? 0,
+    },
+    {
+      Niveau: "M1",
+      Etudiant:
+        item?.[mention.replace(/ /g, "_") as keyof MentionDto]?.data[3]
+          ?.studentNumber ?? 0,
+    },
+    {
+      Niveau: "M2",
+      Etudiant:
+        item?.[mention.replace(/ /g, "_") as keyof MentionDto]?.data[4]
+          ?.studentNumber ?? 0,
+    },
+  ];
 
-const chartConfig = {
-  Etudiant: {
-    label: "Etudiant",
-    color: "hsl(var(--primary))",
-  },
-} satisfies ChartConfig;
+  const fakeData = [
+    { Niveau: "L1", Etudiant: 324 },
+    { Niveau: "L2", Etudiant: 100 },
+    { Niveau: "L3", Etudiant: 43 },
+    { Niveau: "M1", Etudiant: 12 },
+    { Niveau: "M2", Etudiant: 90 },
+  ];
 
-export const BarChartGraph = ({ color }: { color: string }) => {
+  const data = item ? chartData : fakeData;
+
+  const chartConfig = {
+    Etudiant: {
+      label: "Etudiant",
+      color: "hsl(var(--primary))",
+    },
+  } satisfies ChartConfig;
   return (
     <ChartContainer config={chartConfig}>
-      <BarChart accessibilityLayer data={chartData}>
+      <BarChart accessibilityLayer data={data}>
         <CartesianGrid vertical={false} />
         <XAxis
           dataKey="Niveau"
