aa58cd853060198ebe268ab6166d04cd48a0faca
Dera2S <dera.ah.14@gmail.com>
2025-10-05 09:50:24 +0300

    fix : fix mention return type

--- src/features/mention/mention.repository.ts
+++ src/features/mention/mention.repository.ts
@@ -3,6 +3,6 @@ import type { MentionDto } from "./mention.dto";
 import type { UserEntity } from "./user.entity";
 
 export abstract class MentionRepository {
-  abstract getData(): Promise<Result<MentionDto[]>>;
+  abstract getData(): Promise<Result<MentionDto>>;
   abstract register(user: UserEntity): Promise<Result<void>>;
 }
