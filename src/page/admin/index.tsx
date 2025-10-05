b6b5526107ea98e38cd1a48e53f231fc9ffa21de
Dera2S <dera.ah.14@gmail.com>
2025-10-05 13:33:21 +0300

    feat : add sideBar

--- src/page/admin/index.tsx
+++ src/page/admin/index.tsx
@@ -1,20 +1,27 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AdminDashBoardProvider } from "./bloc/useAdminDasboardPortalProvider";
 import { AddUserAndDocument } from "./components/AddUserAndDocument";
 import { MentionCardList } from "./components/MentionListCard";
-import { NavBar } from "./components/nav_bar";
 import { ChartPie } from "./components/PieChart";
+import { AppSidebar } from "@/components/app-sidebar";
+import { NavBar } from "./components/NavBar";
 
 export const AdminDashboardPage = () => {
   return (
     <AdminDashBoardProvider>
-      <div className="flex flex-col w-full p-4 pt-20 bg-zinc-100  dark:bg-zinc-900 transition-all h-full duration-500 ">
-        <NavBar />
-        <div className="flex">
-        <MentionCardList />
-        <ChartPie />
-        </div>
-        <AddUserAndDocument />
-      </div>
+      <SidebarProvider>
+        <AppSidebar variant="inset" />
+        <SidebarInset className=" overflow-hidden ">
+          <NavBar />
+          <div className="flex flex-col w-full p-4  bg-zinc-100  dark:bg-zinc-900 transition-all h-full duration-500 ">
+            <div className="flex  justify-around">
+              <MentionCardList />
+              <ChartPie />
+            </div>
+            <AddUserAndDocument />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
     </AdminDashBoardProvider>
   );
 };
