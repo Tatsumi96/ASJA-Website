788db38a0cd33a133e9a409548487c06e5ec8a6a
Dera2S <dera.ah.14@gmail.com>
2025-10-06 19:33:05 +0300

    feat : add scroll lock

---
+++ src/page/admin/hooks/useScrollLock.ts
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+export const useScrollLock = (isLocked: boolean) => {
+  useEffect(() => {
+    if (isLocked) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => document.body.classList.remove("overflow-hidden");
+  }, [isLocked]);
+};
