d388dc896e14758b2c926fbe97623483a6468df3
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-10-11 15:10:37 +0300

    feat: added page for all field sections

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,12 @@ import { AdminDashboardPage } from "@/page/admin";
 import { LandingPage } from "@/page/landing";
 import { LogInSection } from "@/page/login";
 import { StudentSpacePage } from "@/page/student_space";
+import { InfoPage } from "@/page/Info";
+import { AgroPage } from "@/page/Agro";
+import { LeaPage } from "@/page/LEA";
+import { EcoPage } from "@/page/Eco";
+import { STPage } from "@/page/ST";
+import { DroitPage } from "@/page/Droit";
 import { useTheme } from "@/page/theme/useTheme";
 import "@/i18n";
 
@@ -16,6 +22,12 @@ export const PageRoute = () => {
         <Route path="/login" element={<LogInSection />} />
         <Route path="/admin" element={<AdminDashboardPage />} />
         <Route path="/student-space" element={<StudentSpacePage />} />
+        <Route path="/infoPage" element={<InfoPage />} />
+        <Route path="/agroPage" element={<AgroPage />} />
+        <Route path="/ecoPage" element={<EcoPage />} />
+        <Route path="/leaPage" element={<LeaPage />} />
+        <Route path="/stPage" element={<STPage />} />
+        <Route path="/droitPage" element={<DroitPage />} />
       </Routes>
     </BrowserRouter>
   );
