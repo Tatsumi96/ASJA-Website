42e6ce3fd4c8c973708834faa981ffcc5b996718
MandaHarou <andrainiony0109@gmail.com>
2025-10-13 21:52:49 +0300

    move: remove file event-hackathon

--- src/page/landing/index.tsx
+++ src/page/landing/index.tsx
@@ -1,17 +1,12 @@
 import { Description } from "./components/description";
-import { FiliereSection } from "./components/filiere_section";
-import { MissionSection } from "./components/mission_section";
-import { Navbar } from "./components/navbar";
-import {FaqSection} from "./components/faq_section"
+
 export const LandingPage = () => {
   return (
     <div>
-      <Navbar />
+
       <div className="flex-col items-center justify-center">
         <Description />
-        <MissionSection />
-        <FiliereSection />
-        <FaqSection />
+       
       </div>
     </div>
   );
