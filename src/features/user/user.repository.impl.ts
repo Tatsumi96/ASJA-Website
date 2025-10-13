dd0f53630bdce3cb64341581acc1e6fce4e4aae2
Dera2S <dera.ah.14@gmail.com>
2025-10-13 18:44:01 +0300

    fix : rely to the backend

--- src/features/user/user.repository.impl.ts
+++ src/features/user/user.repository.impl.ts
@@ -1,7 +1,8 @@
-import { failure, success, type Result } from "@/core/result";
-import type { UserDto } from "./user.dto";
-import type { UserRepository } from "./user.repository";
-import type { UserService } from "./user.service";
+import { failure, success, type Result } from '@/core/result';
+import type { UserDto } from './user.dto';
+import type { UserRepository } from './user.repository';
+import type { UserService } from './user.service';
+import { ApiSource } from '@/core/constant';
 
 export class UserRepositoryImpl implements UserRepository {
   constructor(private service: UserService) {}
@@ -9,7 +10,22 @@ export class UserRepositoryImpl implements UserRepository {
   async getData(): Promise<Result<UserDto>> {
     try {
       const result = await this.service.get();
-      return success(result);
+      const userData: UserDto = {
+        identifier: result.identifier,
+        imageUrl: result.imageUrl
+          ? `${ApiSource.url}/mention/stream/${result.imageUrl}`
+          : undefined,
+        name: result.name,
+        lastName: result.lastName,
+        branche: result.branche,
+        level: result.level,
+        Premier: result.Premier,
+        Deuxieme: result.Deuxieme,
+        Troisieme: result.Troisieme,
+        contact: result.contact,
+        mention: result.mention,
+      };
+      return success(userData);
     } catch (error) {
       console.error(error);
       return failure(new Error());
