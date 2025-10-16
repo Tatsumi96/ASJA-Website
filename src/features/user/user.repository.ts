87ac4ad8aceb9768e1102461ddc9445bcdf97cb1
Dera2S <dera.ah.14@gmail.com>
2025-10-16 11:13:49 +0300

    fix : fix user repository

--- src/features/user/user.repository.ts
+++ src/features/user/user.repository.ts
@@ -1,6 +1,8 @@
-import type { Result } from "@/core/result";
-import type { UserDto } from "./user.dto";
+import type { Result } from '@/core/result';
+import type { UserDto } from './user.dto';
+import type { UpdateDto } from './udpate.dto';
 
 export abstract class UserRepository {
   abstract getData(): Promise<Result<UserDto>>;
+  abstract update(user: UpdateDto): Promise<Result<void>>;
 }
