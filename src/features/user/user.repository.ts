43432ce1c567c90a1e5a26a6e4a36c9f7fc8e046
Dera2S <dera.ah.14@gmail.com>
2025-10-16 16:51:34 +0300

    fix : fix update UI of user information

--- src/features/user/user.repository.ts
+++ src/features/user/user.repository.ts
@@ -4,5 +4,5 @@ import type { UpdateDto } from './udpate.dto';
 
 export abstract class UserRepository {
   abstract getData(): Promise<Result<UserDto>>;
-  abstract update(user: UpdateDto): Promise<Result<void>>;
+  abstract update(user: UpdateDto): Promise<Result<UpdateDto>>;
 }
--- src/page/admin/bloc/useAdminDashboard.ts
+++ src/page/admin/bloc/useAdminDashboard.ts
@@ -7,6 +7,7 @@ import type { UserDto } from '@/features/mention/user.dto';
 import type { UserEntity } from '@/features/mention/user.entity';
 import type { PostDto } from '@/features/post/post.dto';
 import type { TrancheDto } from '@/features/tranche/tranche.dto';
+import type { UpdateDto } from '@/features/user/udpate.dto';
 
 import {
   authRepository,
@@ -177,6 +178,31 @@ export const useAdminDashboard = () => {
     });
 
     if (result.status === 'success') {
+      const student = studentList.find(
+        (item) => item.identifier == userMatricule
+      ) as UserDto;
+
+      const newStudentInformationUpdated: UserDto = {
+        ...result.data,
+        Premier: student?.Premier,
+        Deuxieme: student?.Deuxieme,
+        Troisieme: student?.Troisieme,
+        trancheId: student?.trancheId,
+        mentionId: student?.mentionId,
+        fileName: selectedFile?.name as string,
+        imageUrl: result.data.imageUrl
+          ? result.data.imageUrl
+          : student.imageUrl,
+      };
+
+      const oldStudentList = studentList.filter(
+        (item) => item.identifier != userMatricule
+      );
+      setStudentlist(() => [
+        ...oldStudentList,
+        ...[newStudentInformationUpdated],
+      ]);
+
       toast.success('Succes', {
         description: 'Student information updated',
         className: 'animate-fade animate-once animate-ease-out',
