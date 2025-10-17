8e7666e84c616805c7c820b1534e91727cad23ad
Dera2S <dera.ah.14@gmail.com>
2025-10-17 07:38:20 +0300

    feat : mesage developer screen

--- src/page/admin/index.tsx
+++ src/page/admin/index.tsx
@@ -10,6 +10,7 @@ import { Modalprovider } from './bloc/useModalProvider';
 import { Loglist } from './page/log-list';
 import { Postlist } from './page/post-list';
 import { Doclist } from './page/doc-list';
+import { MessageDeveloperScreen } from './components/mesage-developer-screen';
 export const AdminDashboardPage = () => {
   const [index, setIndex] = useState<number>(0);
   const page: JSX.Element[] = [
@@ -26,10 +27,11 @@ export const AdminDashboardPage = () => {
         {' '}
         <SidebarProvider>
           <AppSidebar changePage={setIndex} />
-          <SidebarInset className=" overflow-hidden transition-all duration-500">
+          <SidebarInset className=" overflow-hidden hidden md:flex transition-all duration-500">
             <NavBar />
             {page[index]}
           </SidebarInset>
+          <MessageDeveloperScreen />
         </SidebarProvider>
       </Modalprovider>
     </AdminDashBoardProvider>
