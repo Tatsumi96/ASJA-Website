7c2447e04ffc80f4043347d463778d2f9d6dadd0
MandaHarou <Mandaharou>
2025-10-27 22:34:28 +0300

    add : add handless cms programme academic

---
+++ strapi-docker/app/src/api/programme-academeic/services/programme-academeic.ts
@@ -0,0 +1,7 @@
+/**
+ * programme-academeic service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::programme-academeic.programme-academeic');
