16e71221a5311910cac733d0d8c328c3b8657bdb
Dera2S <dera.ah.14@gmail.com>
2025-10-28 11:55:57 +0300

    fix : fix mention description

--- src/page/Droit/index.tsx
+++ src/page/Droit/index.tsx
@@ -1,37 +1,55 @@
-import { DispositionAboutFiliere } from "@/page/landing/components/disposition-about-filiere";
-import Droit from "@/assets/Mentions/Droit/Droit.jpeg";
-import Image1 from "@/assets/Mentions/Droit/event-droi_conference.jpg";
-import Image3 from "@/assets/Mentions/Droit/event-licence_sortie.jpg";
+import Droit from '@/assets/Mentions/Droit/Droit.jpeg';
+import Image1 from '@/assets/Mentions/Droit/event-droi_conference.jpg';
+import Image3 from '@/assets/Mentions/Droit/event-licence_sortie.jpg';
+import { DispositionAboutFiliere } from '@/page/landing/components/disposition-about-filiere';
 export const DroitPage = () => {
   const mention = {
-    name: "Mention Droit",
+    name: 'Mention Droit',
     ImageMention: Droit,
   };
 
   const descriptionMention = {
-    title: "La Mention Droit",
+    title: 'La Mention Droit',
     description:
       "Formation complète en droit des affaires, droit international et droits de l'homme.",
   };
 
   const descriptionParcours = [
-    { title: "Droit des Affaires", description: "Formation en droit commercial.", categorieParcours: "Droit des Affaires" },
-    { title: "Droit Processuel", description: "Focus sur les procédures juridiques.", categorieParcours: "Droit Processuel" },
-
+    {
+      title: 'Droit des Affaires',
+      description:
+        'Former des juristes d’entreprise et des conseils juridiques Maîtriser le droit commercial, fiscal, des sociétés, contrats… Compétences acquises à la fin de votre formation chez nous : Gestion juridique des sociétés Analyse des risques et conformités Négociations et rédactions des contrats Débouchés professionnels : Fonctionnaire international Notaire spécialisé en sociétés Consultant en fiscalité Juriste d’entreprise Avocat d’affaires',
+      categorieParcours: 'Droit des Affaires',
+    },
+    {
+      title: 'Droit Processuel',
+      description:
+        'Préparer aux concours de barreau, magistrature, greffe Former à toutes les Procédures (civile, pénale, administrative…) Compétences acquises à la fin de votre formation chez nous : Analyse juridique et argumentation Rédaction d’actes juridiques Stratégies procédurales Débouchés professionnels : Assistant(e) de justice Avocat Magistrat Greffier',
+      categorieParcours: 'Droit Processuel',
+    },
   ];
 
   const shortDescriptionList = [
-    { titleEvent: "Hackathon 2023", descriptionEvent: "Victoire de nos étudiants.", RandomImage: Image3 },
-    { titleEvent: "Visite PAMF", descriptionEvent: "Renforcement de la collaboration.", RandomImage: Image1 },
-    
+    {
+      titleEvent: 'Hackathon 2023',
+      descriptionEvent: 'Victoire de nos étudiants.',
+      RandomImage: Image3,
+    },
+    {
+      titleEvent: 'Visite PAMF',
+      descriptionEvent: 'Renforcement de la collaboration.',
+      RandomImage: Image1,
+    },
   ];
 
-  return <div>
-    <DispositionAboutFiliere
-      mention={mention}
-      descriptionMention={descriptionMention}
-      descriptionParcours={descriptionParcours}
-      shortDescriptionList={shortDescriptionList}
-    />
-  </div>;
+  return (
+    <div>
+      <DispositionAboutFiliere
+        mention={mention}
+        descriptionMention={descriptionMention}
+        descriptionParcours={descriptionParcours}
+        shortDescriptionList={shortDescriptionList}
+      />
+    </div>
+  );
 };
