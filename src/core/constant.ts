29a3c5bb2e2898995ac57b4fbd0b5af576250dfc
Dera2S <dera.ah.14@gmail.com>
2025-10-29 08:43:54 +0300

    feat : handle gemini chatbot

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -1,5 +1,5 @@
 export class ApiSource {
   static PORT: number = 3000;
   static url: string = `http://localhost:${this.PORT}`;
-  static strapiUrl: string = 'http://localhost:3000';
+  static strapiUrl: string = 'http://192.168.138:5678';
 }
