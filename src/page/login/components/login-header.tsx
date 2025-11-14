1a7b93b4b21a4e152ca7a94bcece1007901a0178
Dera2S <dera.ah.14@gmail.com>
2025-11-14 11:42:12 +0300

    feat: implement LoginForm component with enhanced UI and functionality; update LoginHeader for language toggle

--- src/page/login/components/login-header.tsx
+++ src/page/login/components/login-header.tsx
@@ -5,7 +5,7 @@ import { Moon, Sun } from 'lucide-react';
 
 export const LoginHeader = () => {
   const { toggleTheme, isDark } = useTheme();
-  const { translate } = useLangue();
+  const { translate, toggleLang, isEn } = useLangue();
   return (
     <div className="flex justify-between w-full top-3 fixed md:px-5 px-2 z-20">
       <a
@@ -20,6 +20,12 @@ export const LoginHeader = () => {
         </div>
       </a>
       <div className="flex items-center">
+        <button
+          className="md:px-5 text-white font-semibold cursor-pointer drop-shadow-md"
+          onClick={toggleLang}
+        >
+          {isEn ? 'FR' : 'EN'}
+        </button>
         <button
           className="px-5 text-white cursor-pointer drop-shadow-md"
           onClick={toggleTheme}
